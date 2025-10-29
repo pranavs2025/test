0e578a3c50e20ead8ad36f66b4711e58acad849a
pranavs2025 <pranav.seenivasagan@karix.com>
2025-10-29 11:44:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config(); // Load .env variables
 
-const PORT = process.env.PORT || 3000;
-const API_KEY = process.env.API_KEY || 'no-key';
+const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
 
 app.get('/', (req, res) => {
   res.send(`Hello! Your API key is: ${API_KEY}`);
